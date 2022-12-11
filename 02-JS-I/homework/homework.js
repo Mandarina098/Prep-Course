89b485abbf9e954e1b33d5c830f3eaddfdb1cbac
Gonzalo <gonzalosilva153@gmail.com>
2022-12-11 01:58:00 -0300

    progreso de funciones

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -77,14 +77,14 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (str1.length === str2.length ) return true ; else return false;
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código: 
-  if (num < 90) return true ; else return false 
+  if (num < 90) return true ; else return false; 
   
 }
 
@@ -92,7 +92,7 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num > 50) return true ; if else return false 
+  if (num > 50) return true ; else return false;
   
 }
 
@@ -106,21 +106,21 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código: 
-  if (num pair ) return true ; if else return false
+  if (num esPar ) return true ; if else return false ;
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ( num not pair ) return true ; if else return false 
+  if ( num esImpar ) return true ; if else return false ;
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  Ñ
 }
 
 function elevarAlCubo(num) {
